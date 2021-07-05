4265d397bffb6511748c751a0b9d46a2ccd67239
ads00 <adshayanbalachandran@gmail.com>
2021-07-05 23:23:53 +0530

    Display of repositories done

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ searchUser.addEventListener('keyup', (e) => {
                     // Show Profile
                     ui.clearAlert();
                     ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }
             });
     } else {
--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = 'f5ccbb96646c040f4ef6';
         this.client_secret = '488ccc5d5821811a5200793ee73859d72219aadd';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         };
     }
 }
